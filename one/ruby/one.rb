d771b1787d021fb62c31e0202ac73e4c4db6ee81
Matthew Barrington <edeninflames@gmail.com>
2020-12-04 15:10:16 +0100

    Update Day 1 to just use combinarions

--- one/ruby/one.rb
+++ one/ruby/one.rb
@@ -2,31 +2,17 @@
 
 TOTAL = 2020
 
-def two(numbers)
-  hash = {}
-
-  numbers.each do |num|
-    hash[num.to_i] = 1
-  end
-
-  hash.each do |k,v|
-    next unless hash.key?(TOTAL - k)
-
-    return k * (TOTAL - k)
-  end
-end
-
-def three(numbers)
-  numbers.combination(3).each do |group|
-    return group.inject(1, :*) if group.sum == TOTAL
+def combo(numbers, combination)
+  numbers.combination(combination).each do |group|
+    return group.inject(:*) if group.sum == TOTAL
   end
 end
 
 def main
   numbers = File.readlines("../input.txt").map(&:to_i).sort
 
-  puts two(numbers)
-  puts three(numbers)
+  puts combo(numbers, 2)
+  puts combo(numbers, 3)
 end
 
 main
