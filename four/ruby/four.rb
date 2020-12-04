f62880ab9ee8920540cb607e19eb7875c9eebf27
Matthew Barrington <edeninflames@gmail.com>
2020-12-04 11:52:08 +0000

    Small logic bug exists

--- four/ruby/four.rb
+++ four/ruby/four.rb
@@ -6,9 +6,9 @@ class Identification
   attr_reader :byr, :iyr, :eyr, :hgt, :hcl, :ecl, :pid, :cid, :total
 
   def initialize(data)
-    @byr = data["byr"]
-    @iyr = data["iyr"]
-    @eyr = data["eyr"]
+    @byr = data["byr"].to_i
+    @iyr = data["iyr"].to_i
+    @eyr = data["eyr"].to_i
     @hgt = data["hgt"]
     @hcl = data["hcl"]
     @ecl = data["ecl"]
@@ -17,10 +17,66 @@ class Identification
     @total = data.length
   end
 
-  def valid?
-    return true if total == 8
+  def valid_byr?
+    (1920..2002).include?(byr)
+  end
 
-    return true if total == 7 && cid.nil?
+  def valid_iyr?
+    (2010..2020).include?(iyr)
+  end
+
+  def valid_eyr?
+    (2020..2030).include?(eyr)
+  end
+
+  def valid_hgt?
+    height = hgt[0...-2].to_i
+    unit = hgt[-2..-1]
+
+    return (150..193).include?(height) if unit == "cm"
+    return (59..76).include?(height) if unit == "in"
+
+    false
+  end
+
+  def valid_hcl?
+    hcl.match?(/#[0-9a-f]{6}/)
+  end
+
+  def valid_ecl?
+    %w[amb blu brn gry grn hzl oth].include?(ecl)
+  end
+
+  def valid_pid?
+    pid.match?(/\d{9}/)
+  end
+
+  def all_fields?
+    total == 8
+  end
+
+  def just_missing_cid?
+    total == 7 && cid.nil?
+  end
+
+  def old_valid?
+    return true if all_fields? || just_missing_cid?
+
+    false
+  end
+
+  def new_valid?
+    if all_fields? || just_missing_cid?
+      return false unless valid_byr?
+      return false unless valid_iyr?
+      return false unless valid_eyr?
+      return false unless valid_hgt?
+      return false unless valid_hcl?
+      return false unless valid_ecl?
+      return false unless valid_pid?
+
+      return true
+    end
 
     false
   end
@@ -50,7 +106,8 @@ end
 ids = ids.map { |x| x.join(" ") }
 
 
-valid = 0
+old_valid = 0
+new_valid = 0
 
 ids.each do |id|
   data = {}
@@ -63,7 +120,9 @@ ids.each do |id|
 
   identification = Identification.new(data)
 
-  valid += 1 if identification.valid?
+  old_valid += 1 if identification.old_valid?
+  new_valid += 1 if identification.new_valid?
 end
 
-puts valid
+puts old_valid
+puts new_valid
