2981efd3755fdb1848444541dda431b7a47d09c3
Matthew Barrington <edeninflames@gmail.com>
2020-12-06 10:05:14 +0000

    Clean up Day 6
    Turns out you can pass a block to count :O

--- six/ruby/six.rb
+++ six/ruby/six.rb
@@ -1,17 +1,16 @@
 #!/usr/bin/env ruby
 groups = File.open("../input.txt").read.chomp.split("\n\n")
 
-first_count = 0
-second_count = 0
+first_count, second_count = 0, 0
 
 groups.each do |group|
   party_size = group.split.count
 
   answers = group.gsub("\n", "").chars.tally
 
-  first_count += answers.keys.count
+  first_count += answers.values.count
 
-  second_count += answers.values.map { |val| val >= party_size }.tally[true].to_i
+  second_count += answers.values.count { |val| val >= party_size }
 end
 
 puts first_count
