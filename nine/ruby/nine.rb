1810be3cdb967dcddd71628657a8dc8e34db5e90
Matthew Barrington <edeninflames@gmail.com>
2020-12-09 08:48:13 +0000

    Slight change to prep for day 9 part 2

--- nine/ruby/nine.rb
+++ nine/ruby/nine.rb
@@ -1,17 +1,18 @@
 #!/usr/bin/env ruby
 
-lines = File.readlines("../input.txt", chomp: true).map(&:to_i)
+lines = File.readlines("../test.txt", chomp: true).map(&:to_i)
 
-window = 25
+window = 5
 
 previous = lines[0, window]
 
 to_check = 0
+bad_number = nil
 
 lines[window..-1].each_cons(window) do |con|
   to_check = con.first
-  break to_check unless previous.combination(2).map(&:sum).include?(to_check)
+  bad_number = to_check unless previous.combination(2).map(&:sum).include?(to_check)
   previous = previous[1..-1] << to_check
 end
 
-puts to_check
+puts bad_number
