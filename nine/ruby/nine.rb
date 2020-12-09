3e950ceab1ba9fc075cda60bef91fa192b619362
Matthew Barrington <edeninflames@gmail.com>
2020-12-09 09:09:36 +0000

    Added part 2

--- nine/ruby/nine.rb
+++ nine/ruby/nine.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
+lines = File.readlines("../input.txt", chomp: true).map(&:to_i)
 
-lines = File.readlines("../test.txt", chomp: true).map(&:to_i)
-
-window = 5
+window = 25
 
 previous = lines[0, window]
 
@@ -11,8 +10,20 @@ bad_number = nil
 
 lines[window..-1].each_cons(window) do |con|
   to_check = con.first
-  bad_number = to_check unless previous.combination(2).map(&:sum).include?(to_check)
+  unless previous.combination(2).map(&:sum).include?(to_check)
+    bad_number = to_check
+    break
+  end
   previous = previous[1..-1] << to_check
 end
 
 puts bad_number
+
+(0..lines.size).each do |i|
+  (i..lines.size).each do |j|
+    sub_array = lines[i..j]
+    if sub_array.sum == bad_number && sub_array.length > 1
+      puts sub_array.max + sub_array.min
+    end
+  end
+end
