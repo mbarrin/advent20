c566f44ffdd8edb2b5fa7820cfd30501f469a6be
Matthew Barrington <matthew@barrington.it>
2020-12-11 09:30:02 +0000

    Figuring it out

--- ten/ruby/ten.rb
+++ ten/ruby/ten.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
 #
-def main
-  lines = File.readlines("../input.txt", chomp: true).map(&:to_i).sort
-
+require 'pry'
+def part1(lines)
   outlet = 0
 
   tracker = { 1 => 0, 2 => 0, 3 => 0 }
@@ -11,21 +10,41 @@ def main
     if lines.include?(outlet+1)
       outlet += 1
       tracker[1] += 1
-      next
     elsif lines.include?(outlet+2)
       outlet += 2
       tracker[2] += 1
-      next
     elsif lines.include?(outlet+3)
       outlet += 3
       tracker[3] += 1
-      next
     end
   end
 
   tracker[3] += 1
 
-  puts tracker[3] * tracker[1]
+  tracker[3] * tracker[1]
 end
+
+def part2(lines)
+  fork_indexes = {}
+  fork_count = 0
+  lines.to_enum.with_index.reverse_each do |line, index|
+    foo = [1,2,3].map do |i|
+      index+i if lines.include?(line+i)
+    end
+    foo = foo.compact
+    if foo.size >= 2
+      fork_indexes[index] = foo
+    end
+  end
+  fork_indexes
+end
+
+def main
+  lines = File.readlines("../test.txt", chomp: true).map(&:to_i).sort
+
+  # puts part1(lines)
+  puts part2(lines)
+end
+
 
 main
